831070ad7585156306b2a2ed36e28e4f87457419
darkhankz <upstudynow@gmail.com>
2022-08-24 13:28:38 +0600

    Change-XML1

---
+++ app/src/main/java/com/example/mygitproject/presentation/ShopItemActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mygitproject.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mygitproject.R
+
+class ShopItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shop_item)
+    }
+}
